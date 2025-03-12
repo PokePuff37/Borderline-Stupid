c74f58d3e6a80dcc34f462c2582f32c69cea179e
PokePuff05 <nataliehancock38@gmail.com>
2025-03-12 10:26:48 -0400

    Started The Commands

---
+++ src/main/java/com/gmail/pokepuff37/bls/commands/BorderLineStupidCommands.java
@@ -0,0 +1,11 @@
+package com.gmail.pokepuff37.bls.commands;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BorderLineStupidCommands implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
